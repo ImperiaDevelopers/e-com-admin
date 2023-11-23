8b6ee876b58bc3d0465c8e75db924601cd08c432
sardorixtiyorov <sardorixtiyorov0116@gmail.com>
2023-11-23 11:19:35 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="w-[1180px] h-[100%] bg-black">
+    
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dblue: "#12486B",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [],
+};
